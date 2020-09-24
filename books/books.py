78a1b04632e5a0ed5789a82ac7cbddf36b1c6854
acameron13 <63213231+acameron13@users.noreply.github.com>
2020-09-23 21:19:30 -0500

    Done parsing arguments!

--- books/books.py
+++ books/books.py
@@ -3,6 +3,7 @@ import csv
 import sys
 
 def print_usage():
+    print("printing usage")
     #print each line of usage.txt
     return 0
 
@@ -37,9 +38,9 @@ def find_books_in_range(startyear, endyear):
     return 0
 
 def parse_arguments(arg_list):
-
+    arg_dict = {}
     if (len(arg_list) <= 1) or ('-d' in arg_list and len(arg_list) != 4):
-        return valid
+        valid = False
 
     operation_count = 0
     for arg in arg_list:
@@ -47,6 +48,9 @@ def parse_arguments(arg_list):
             operation_count += 1
             if '=' in arg and len(arg.split('=')) == 2:
                 valid = True
+                searchstring = arg.split('=')[1]
+                arg_dict["command"] = '-a'
+                arg_dict["searchstring"] = searchstring
             else:
                 print("The option -a must include a SEARCHSTRING")
                 print("See below for proper command-line syntax \n\n")
@@ -54,25 +58,48 @@ def parse_arguments(arg_list):
 
         if '-d' in arg or '--daterange' in arg:
             operation_count += 1
-            #do more checking
+            arg_dict["command"] = '-d'
             from_count = 0
-            to_count = 0
+            until_count = 0
             for daterange_arg in arg_list:
-                if '--from'in daterange_arg:
+                if '--from' in daterange_arg or "--until" in daterange_arg:
+                    if '--from' in daterange_arg:
+                        from_count += 1
+                    elif '--until' in daterange_arg:
+                        until_count += 1
+
                     if '=' in daterange_arg and len(daterange_arg.split('=')) == 2:
                         try:
                             year = int(daterange_arg.split('=')[1])
-                        catch
-                    from_count
+                            if '--from' in daterange_arg:
+                                arg_dict["startyear"] = year
+                            elif '--until' in daterange_arg:
+                                arg_dict["endyear"] = year
+                        except:
+                            print("Make sure your STARTYEAR and ENDYEAR are valid integers.\n\n")
+                    else:
+                        print("The option -d must include both --from=STARTYEAR and --until=ENDYEAR.\n\n")
+
+            if (from_count == 1 and until_count == 1) and ("startyear" in arg_dict) and ("endyear" in arg_dict):
+                valid = True
+                if arg_dict["startyear"] >= arg_dict["endyear"]:
+                    print("For the option -d, STARTYEAR must be earlier than ENDYEAR.\n\n")
+                    valid = False
+            else:
+                valid = False
 
         if '-h' in arg or '--help' in arg:
             operation_count += 1
             valid = True
+            arg_dict["command"] = '-h'
 
         if '-t' in arg or '--title' in arg:
             operation_count += 1
             if '=' in arg and len(arg.split('=')) == 2:
                 valid = True
+                searchstring = arg.split('=')[1]
+                arg_dict["command"] = '-t'
+                arg_dict["searchstring"] = searchstring
             else:
                 print("The option -t must include a SEARCHSTRING")
                 print("See below for proper command-line syntax \n\n")
@@ -82,15 +109,25 @@ def parse_arguments(arg_list):
     #users can only use one command at a time
     if operation_count != 1:
         valid = False
+        if operation_count > 1:
+            print("You can only execute one command at once.")
+        elif operation_count == 0:
+            print("You did not enter any valid command line arguments.")
+        print("See usage page below. \n\n")
 
-    return valid
+    arg_dict["valid"] = valid
+    return arg_dict
 
 def main():
     #parse command-line arguments
     arg_list = sys.argv
     print(sys.argv)
-    if not(are_valid_arguments):
+    parsed_args = parse_arguments(arg_list)
+    print(parsed_args)
+    if not(parsed_args["valid"]) or parsed_args["command"] == '-h':
         print_usage()
+        return
+
 
     #verify that command-line arguments are valid
     #depending on the arguments, run the proper functions
