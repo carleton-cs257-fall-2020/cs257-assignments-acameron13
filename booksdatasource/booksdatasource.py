ff1598fb9f234a15dca2c0bdfd1d52401e027a4f
acameron13 <63213231+acameron13@users.noreply.github.com>
2020-10-05 20:11:01 -0500

    tested my tests

--- booksdatasource/booksdatasource.py
+++ booksdatasource/booksdatasource.py
@@ -6,59 +6,23 @@
     For use in some assignments at the beginning of Carleton's
     CS 257 Software Design class, Fall 2020.
 
-    An instance of the BooksDataSource class described below is intended
-    to provide access to data about books and authors. The particular form
-    in which the books and authors are stored may depend on the context,
-    but the method interfaces will remain unchanged from context to context.
-
-    This is intended in part as a reminder, post-CS201, that any given
-    class or function interface might have many different implementations.
-    (Think about Java, where both ArrayList and LinkedList provide
-    implementations of the List interface, but with different time complexity
-    for various operations.)
-
-    This class's two methods (not including __init__) return lists of
-    dictionaries, each dictionary representing either a book or an author.
-
-    Each author is represented as a dictionary with the keys
-    'id', 'last_name', 'first_name', 'birth_year', and 'death_year'.
-    An author's ID is an arbitrary integer, whose only constraint is that
-    different authors must have different IDs. For example, Jane Austen
-    would be represented like this (assuming her ID number is 72):
+    Author format:
 
         {'id': 72, 'last_name': 'Austen', 'first_name': 'Jane',
          'birth_year': 1775, 'death_year': 1817}
-
-    For a living author, the death_year is represented in the author's
-    Python dictionary as None.
-
         {'id': 134, 'last_name': 'Murakami', 'first_name': 'Haruki',
          'birth_year': 1949, 'death_year': None}
 
-    Note that this is a simple-minded representation of a person in
-    several ways. For example, how do you represent the birth year
-    of Sophocles? What is the last name of Gabriel García Márquez?
-    Should we refer to the author of "Tom Sawyer" as Samuel Clemens or
-    Mark Twain? Are Voltaire and Molière first names or last names? etc.
-
-    A book is represented as a dictionary with the keys 'title',
-    'publication_year', and 'author_id'. For example, "A Wild Sheep Chase"
-    would look like this:
+    Book format:
 
         {'title': 'A Wild Sheep Chase', 'publication_year': 1982, 'author_id': 134}
-
-    Note that unlike in your first books-related assignment, books accessible
-    through a BooksDataSource object may only have one author. (So sad, no
-    "Good Omens" for us.) We'll take a closer look at this issue in a later
-    assignment.
 '''
+
+import csv
 
 class BooksDataSource:
     def __init__(self, books_csv_file_name, authors_csv_file_name):
-        ''' Initializes whatever instance variables are required to implement the
-            other methods in this class.
-            
-            The books CSV file format:
+        ''' The books CSV file format:
 
                 author_id,title,publication_year
                 4,Jane Eyre,1847
@@ -69,10 +33,30 @@ class BooksDataSource:
                 23,Carré,John Le,1931,NULL
                 18,DuMaurier,Daphne,1907,1989
 
-            Note that NULL is used to represent a non-existent (or rather, future and
-            unknown) year in the cases of living authors.
+            NULL is used in the CSV to represent a future/unknown death year.
         '''
-        pass
+        self.book_list = []
+        with open(books_csv_file_name) as books_file:
+            reader = csv.reader(books_file)
+            row = next(reader) # consume the top row and ignore it
+            for row in reader:
+                book = {'author_id':int(row[0]), 'title':row[1], 'publication_year': int(row[2])}
+                self.book_list.append(book)
+
+        self.author_ids = {} # this will help us check an author ID's validity quickly
+        self.author_list = []
+        with open(authors_csv_file_name) as authors_file:
+            reader = csv.reader(authors_file)
+            row = next(reader)
+            for row in reader:
+                death_year = int(row[4]) if row[4] != 'NULL' else None
+                author = {'id':int(row[0]), 'last_name':row[1], 'first_name':row[2],
+                          'birth_year':int(row[3]), 'death_year':death_year}
+                self.author_ids[author['id']] = 0
+                self.author_list.append(author)
+
+    def is_valid(self, author_id):
+        return author_id in self.author_ids
 
     def books(self, *, author_id=None, search_text=None, start_year=None, end_year=None, sort_by='title'):
         ''' Returns a list of all the books in this data source matching all of
@@ -83,68 +67,44 @@ class BooksDataSource:
                 start_year - only returns books published during or after this year
                 end_year - only returns books published during or before this year
 
-            Note that parameters with value None do not affect the list of books returned.
-            Thus, for example, calling books() with no parameters will return a list of
-            dictionaries representing all the books in the data source.
-
             The list of books is sorted in an order depending on the sort_by parameter:
 
                 'year' -- sorts by publication_year, breaking ties with (case-insenstive) title
                 default -- sorts by (case-insensitive) title, breaking ties with publication_year
-                
-            See the BooksDataSource comment for a description of how a book is represented.
 
             This method raises a ValueError if author_id is non-None but is not a valid author ID.
         '''
-        return []
+        filtered_books = []
+        for book in self.book_list:
+            if author_id is not None and not self.is_valid(author_id):
+                raise ValueError(f'Invalid author ID {author_id}')
+            if author_id is not None and book['author_id'] != author_id:
+                continue
+            if search_text is not None and search_text.lower() not in book['title'].lower():
+                continue
+            if start_year is not None and book['publication_year'] < start_year:
+                continue
+            if end_year is not None and book['publication_year'] > end_year:
+                continue
+            filtered_books.append(book)
+
+        if sort_by == 'year':
+            filtered_books = sorted(filtered_books, key=lambda book: book['publication_year'])
+        else:
+            filtered_books = sorted(filtered_books, key=lambda book: book['title'])
+
+        return filtered_books.copy()
 
     def authors(self, search_text=None):
         ''' Returns a list of all the authors in this data source whose names contain
             (case-insensitively) the search text. If search_text is None, then authors
             returns all of the authors, sorted by birth year.
-
-            See the BooksDataSource comment for a description of how an author is represented.
         '''
-        return []
-
-
-'''
-    Some things to think about.
-
-    1. Here are some examples :
-
-        data_source = BooksDataSource()
-        books = data_source.books(search_text='the')
-
-    The books variable should now refer to a list like this:
-    
-        [{'title':'The Fire Next Time',...}, {'title':'There, There',...},...]
-
-    with one dictionary per book.
-
-    Similarly:
-
-        data_source = BooksDataSource()
-        authors = data_source.authors(search_text='ill')
-
-    should give us a list something like this:
-
-        [{'id':27, 'last_name':'Melville',...}, {'id':142, 'last_name':'Willis',...},...]
-
-    One way to get comfortable with an interface is to generate examples like this to
-    see if they make sense to you.
-
-    2. Do you know what it means to "raise an exception" in Python? If not, ask me or do some research.
-       (By the way, it is common to use the verb "throw" instead of "raise" when talking about
-       exceptions in general, but Python's exception syntax uses "raise".)
-
-    3. QUESTION: Should the books() and authors() interfaces raise TypeError, and if so, when?
-       For example, books() could raise a TypeError if author_id, start_year, or end_year are
-       not None but not of type int, or if search_text is not None but not of type str.
-       That is, we could raise TypeError any time a parameter has an unexpected type.
-       ANSWER, FOR NOW: We won't do this for this assignment, but it deserves discussion.
-
-    4. QUESTION: How about ValueError? And if so, for which parameters?
-       ANSWER: note that we've specified a ValueError in books() if author_id is non-None.
-       For now, we won't try to raise ValueError in any other context.
-'''
+        filtered_authors = []
+        for author in self.author_list:
+            name = author['first_name'] + ' ' + author['last_name']
+            if search_text is not None and search_text.lower() not in name.lower():
+                continue
+            filtered_authors.append(author)
+        filtered_authors = sorted(filtered_authors, key=lambda author: author['birth_year'])
+        return filtered_authors.copy()
